e88832a2b312b26ce251a4aa2d86f16a6e63b7f1
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-09 16:25:02 +0200

    do task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItem(galleryItems);
+galleryRef.innerHTML = galleryItemsMarkup;
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.defaultOptions.captionsData = 'alt';
+gallery.defaultOptions.captionDelay = 250;
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>`,
+    )
+    .join('');
+}
